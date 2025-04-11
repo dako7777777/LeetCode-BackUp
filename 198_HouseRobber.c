3ac800c3f146ea4a4f946395f1041b77a2134fdc
dako7777777 <curiosiky@gmail.com>
2025-04-10 18:37:39 -0700

    right version

--- 198_HouseRobber.c
+++ 198_HouseRobber.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+// previous version mistakenly think there is only 2 combination of items
 
-void helper(int *i, int size, int *sum, int *arr) {
-  // base case
-  if (*i >= size - 1) {
-    return;
+// Meaning: item in index i represent
+// "what's the maximum possible robbery value if I start considering houses from
+// position i onwards?
+int memo[100];
+
+int dp(int i, int *nums, int numsSize) {
+  // Base case
+  if (i >= numsSize) {
+    return 0;
   }
 
-  // recursion part
-  *i += 2;
-  *sum += arr[*i];
-  helper(i, size, sum, arr);
+  // Check if already computed
+  if (memo[i] != -1) {
+    return memo[i];
+  }
+
+  // Two choices: rob current house or skip it
+  int rob_current = nums[i] + dp(i + 2, nums, numsSize);
+  int skip_current = dp(i + 1, nums, numsSize);
+
+  // Save result in memo table
+  memo[i] = (rob_current > skip_current) ? rob_current : skip_current;
+
+  return memo[i];
 }
 
 int rob(int *nums, int numsSize) {
-  int sum1 = 0;
-  int sum2 = 0;
-  int index1 = -2;
-  int index2 = -1;
+  // Initialize memo table with -1
+  for (int i = 0; i < numsSize; i++) {
+    memo[i] = -1;
+  }
 
-  helper(&index1, numsSize, &sum1, nums);
-  helper(&index2, numsSize, &sum2, nums);
-
-  return (sum1 > sum2) ? sum1 : sum2;
+  return dp(0, nums, numsSize);
 }
